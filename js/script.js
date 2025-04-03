1014f5d25bca327dd61797db8f29a7f28f691cfb
verin90 <xemiohax223@gmail.com>
2025-04-03 10:34:08 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('click', function(event) {
+let id = event.target.dataset.toggleId;
+if (!id) return;
+let elem = document.getElementById(id);
+elem.hidden = !elem.hidden;
+});
